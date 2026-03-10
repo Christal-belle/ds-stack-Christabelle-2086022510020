138f8512efddb9d637a07ab7e98f7e85838b8995
Christabelle Halim <chritabellesawo15@gmail.com>
2026-03-11 01:21:43 +0800

    update no 3, 4

--- question 4/SecuritySystem.java
+++ question 4/SecuritySystem.java
@@ -1,5 +1,30 @@
-package question 4;
+import java.util.*;
 
 public class SecuritySystem {
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        while (sc.hasNextInt()) {
+            stack.push(sc.nextInt());
+        }
+
+        while (!stack.isEmpty()) {
+            arr.add(stack.pop());
+        }
+
+        Collections.sort(arr);
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            stack.push(arr.get(i));
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
 }
+
